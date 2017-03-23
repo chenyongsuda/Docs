052f04621a5340dd79380c94796ffddbb61b2da3
TonyChen <chenyongsuda@163.com>
2017-03-23 20:21:04 +0800

    Add files via upload

---
+++ Tech/Java/Concurrent/StanderForkJoinCode/Test.java
@@ -0,0 +1,27 @@
+package org.logicalcobwebs.concurrent;
+
+public class Test {
+	public static void main(String[] args) {
+		try {
+			int groupSize = 4; // for example
+			FJTaskRunnerGroup group = new FJTaskRunnerGroup(groupSize);
+			Fib f = new Fib(1,8); // for example
+			group.invoke(f);
+			System.out.println("----------------------------");
+			Fib f1 = new Fib(1,8); // for example
+			group.invoke(f1);
+			System.out.println("----------------------------");
+			Fib f2 = new Fib(1,8); // for example
+			group.invoke(f2);
+//			int result = f.getAnswer();
+//			System.out.println("Answer: " + result);
+			
+			
+//			int result1 = f1.getAnswer();
+//			System.out.println("Answer: " + result1);
+//			int result2 = f2.getAnswer();
+//			System.out.println("Answer: " + result2);
+		} catch (InterruptedException ex) {
+		}
+	}
+}
